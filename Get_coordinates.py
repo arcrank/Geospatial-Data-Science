da29a6d1a7c2d886c437726b94cfc50d2917cd11
arcrank <arcrank@gmail.com>
2017-10-04 12:31:52 -0700

    Add files via upload

--- Get_coordinates.py
+++ Get_coordinates.py
@@ -6,8 +6,8 @@ import numpy as np
 
 
 #Import a dataframe that includes Addresses
-
-
+addr1= '1600 Pennsylvania Ave, Baltimore, MD 21217'
+addr2='1600 Pennsylvania Ave NW, Washington, DC 20500'
 
 df = pd.DataFrame([['name1',addr1],['name2',addr2]],columns=['Name','Address']) #example
 
@@ -40,3 +40,22 @@ for ix in df.index:
         pass
 
 df['Coordinates'] = coordinates
+print(df.head)
+
+
+
+def MapCreator(df):
+    m = folium.Map()#location=[latitudes[0],longitudes[0]])
+    tooltip = 'Click me!'
+    for index,row in df.iterrows():
+        coords = dict(row['Coordinates'])
+        lat = coords['lat']
+        lng = coords['lng']
+        popup_text = row['Name']
+        popup_str = '<i>'+popup_text+'</i>'
+        print(popup_str)
+
+        folium.Marker([lat, lng], popup=popup_str).add_to(m)
+    m.save('index.html')
+
+MapCreator(df)
